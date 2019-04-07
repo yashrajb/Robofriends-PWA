6cc596525bb3b446896b9ab376cb70bb48aede8d
yashrajb <imyashraj.basan@gmail.com>
2019-04-07 18:51:48 +0530

    Updates

---
+++ precache-manifest.29dfea9f5231e505df1a6b4af792df54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c05f5728232266ef25b8",
+    "url": "/Robofriends-PWA/static/js/runtime~main.c05f5728.js"
+  },
+  {
+    "revision": "a0e22eb04cd5b9ae9d63",
+    "url": "/Robofriends-PWA/static/js/main.a0e22eb0.chunk.js"
+  },
+  {
+    "revision": "e12431ffc2bfa8c594d9",
+    "url": "/Robofriends-PWA/static/js/2.e12431ff.chunk.js"
+  },
+  {
+    "revision": "a0e22eb04cd5b9ae9d63",
+    "url": "/Robofriends-PWA/static/css/main.0f9d5207.chunk.css"
+  },
+  {
+    "revision": "e12431ffc2bfa8c594d9",
+    "url": "/Robofriends-PWA/static/css/2.54268b35.chunk.css"
+  },
+  {
+    "revision": "e54674b8a75880bb8fe126983329bc77",
+    "url": "/Robofriends-PWA/index.html"
+  }
+];
