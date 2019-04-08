d6678f6882a19c09da7f98d8755f7fa3dcba4215
yashrajb <imyashraj.basan@gmail.com>
2019-04-08 12:36:14 +0530

    aria label

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -3,9 +3,7 @@ import React from "react";
 const Searchbox = (props) => {
 return (
     <div>
-      <label for="searchbox"></label>
-      <input type="text" name="searchbox" id="searchbox" className="pa2 ma3" placeholder="Search..." onInput={props.onTextChange}/>
-      
+      <input type="text" aria-label="search box" name="searchbox" id="searchbox" className="pa2 ma3" placeholder="Search..." onInput={props.onTextChange}/>
     </div>
 );
 }
