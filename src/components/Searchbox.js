08a29ed84c2469a243debb692556f7ee6d3e3589
yashrajb <imyashraj.basan@gmail.com>
2019-04-08 12:26:33 +0530

    label

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -3,7 +3,8 @@ import React from "react";
 const Searchbox = (props) => {
 return (
     <div>
-      <label for="searchbox"><input type="text" name="searchbox" id="searchbox" className="pa2 ma3" placeholder="Search..." onInput={props.onTextChange}/></label>
+      <label for="searchbox"></label>
+      <input type="text" name="searchbox" id="searchbox" className="pa2 ma3" placeholder="Search..." onInput={props.onTextChange}/>
       
     </div>
 );
